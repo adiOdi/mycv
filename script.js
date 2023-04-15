8f386b76496b1c7ffc618d4ae43b69a79f6e77a8
AdiOdi <git@ondasadriel.com>
2023-04-15 13:18:17 +0200

    format

--- script.js
+++ script.js
@@ -4,26 +4,26 @@ async function encryptMessage(key, message) {
     let encoded = enc.encode(message);
     counter = window.crypto.getRandomValues(new Uint8Array(16));
     ciphertext = await window.crypto.subtle.encrypt(
-    {
-        name: "AES-CTR",
-        counter,
-        length: 64
-    },
-    key,
-    encoded
+        {
+            name: "AES-CTR",
+            counter,
+            length: 64
+        },
+        key,
+        encoded
     );
     return ciphertext;
 }
 
 async function decryptMessage(key, ciphertext) {
     let decrypted = await window.crypto.subtle.decrypt(
-    {
-        name: "AES-CTR",
-        counter,
-        length: 64
-    },
-    key,
-    ciphertext
+        {
+            name: "AES-CTR",
+            counter,
+            length: 64
+        },
+        key,
+        ciphertext
     );
 
     let dec = new TextDecoder();
@@ -33,24 +33,24 @@ async function decryptMessage(key, ciphertext) {
 async function exportCryptoKey(key) {
     const exported = await window.crypto.subtle.exportKey("raw", key);
     console.log(window.base64url.encode(exported));
-  }
+}
 
 window.crypto.subtle
-.generateKey(
-    {
-    name: "AES-CTR",
-    length: 256
-    },
-    true,
-    ["encrypt", "decrypt"]
-)
-.then((key) => {
-    encryptMessage(key, 'plop').then((cipher)=>{
-        decryptMessage(key, cipher).then((m)=>{
-            console.log(m);
+    .generateKey(
+        {
+            name: "AES-CTR",
+            length: 256
+        },
+        true,
+        ["encrypt", "decrypt"]
+    )
+    .then((key) => {
+        encryptMessage(key, 'plop').then((cipher) => {
+            decryptMessage(key, cipher).then((m) => {
+                console.log(m);
+            });
         });
+        exportCryptoKey(key);
     });
-    exportCryptoKey(key);
-});
-  
+
 
