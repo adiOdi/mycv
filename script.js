6572c393b492d8dd2be300e1f61dd6657b78d304
AdiOdi <git@ondasadriel.com>
2023-04-15 13:31:32 +0200

    importing key

--- script.js
+++ script.js
@@ -32,10 +32,21 @@ async function decryptMessage(key, ciphertext) {
 
 async function exportCryptoKey(key) {
     const exported = await window.crypto.subtle.exportKey("raw", key);
-    console.log(window.base64url.encode(exported));
+    return(window.base64url.encode(exported));
+}
+async function importCryptoKey(base64) {
+    const buffer = window.base64url.decode(base64);
+    return await window.crypto.subtle.importKey("raw", buffer,
+    {
+        name: "AES-CTR",
+        length: 256
+    },
+    true,
+    ["encrypt", "decrypt"]);
 }
 
-window.crypto.subtle
+async function main() {
+    let key = await window.crypto.subtle
     .generateKey(
         {
             name: "AES-CTR",
@@ -43,14 +54,13 @@ window.crypto.subtle
         },
         true,
         ["encrypt", "decrypt"]
-    )
-    .then((key) => {
-        encryptMessage(key, 'plop').then((cipher) => {
-            decryptMessage(key, cipher).then((m) => {
-                console.log(m);
-            });
-        });
-        exportCryptoKey(key);
-    });
+    );
+    let kb64 = await exportCryptoKey(key);
+    console.log(kb64);
+    let importedKey = await importCryptoKey(kb64);
+    let cipher = await encryptMessage(importedKey, "plop");
+    console.log(await decryptMessage(key,cipher));
+}
+main();
 
 
