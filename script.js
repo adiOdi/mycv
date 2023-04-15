f8f433cda25f560cf789daac91de0a4060e5a929
AdiOdi <git@ondasadriel.com>
2023-04-15 18:37:01 +0200

    reading encrypted json

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let counter;
+let counter=new Uint8Array(16)//window.crypto.getRandomValues(new Uint8Array(16));
 async function encryptMessage(key, message) {
     let enc = new TextEncoder();
     let encoded = enc.encode(message);
-    counter = window.crypto.getRandomValues(new Uint8Array(16));
     ciphertext = await window.crypto.subtle.encrypt(
         {
             name: "AES-CTR",
@@ -45,31 +44,67 @@ async function importCryptoKey(base64) {
     ["encrypt", "decrypt"]);
 }
 
-async function main(kb64) {
-    // let key = await window.crypto.subtle
-    // .generateKey(
-    //     {
-    //         name: "AES-CTR",
-    //         length: 256
-    //     },
-    //     true,
-    //     ["encrypt", "decrypt"]
-    // );
-    // let kb64 = await exportCryptoKey(key);
-    // console.log(kb64);
-    let importedKey = await importCryptoKey(kb64);
-    let cipher = await encryptMessage(importedKey, "plop");
-    console.log(await decryptMessage(importedKey, cipher));
+// not really neccessary as one can just put any b64url text here
+async function createKey() {
+    let key = await window.crypto.subtle
+    .generateKey(
+        {
+            name: "AES-CTR",
+            length: 256
+        },
+        true,
+        ["encrypt", "decrypt"]
+    );
+    let kb64 = await exportCryptoKey(key);
+    console.log(kb64);
 }
 
-
+async function decrypt(kb64) {
+    let importedKey = await importCryptoKey(kb64);
+    let cipher = await window.base64url.decode(encryptedHtml)
+    return await decryptMessage(importedKey, cipher);
+}
+async function encrypt(kb64) {
+    const response=await fetch('./data.json');
+    const data=await response.json();
+    let importedKey = await importCryptoKey(kb64);
+    let cipher = await encryptMessage(importedKey, JSON.stringify(data));
+    let cipherText=await window.base64url.encode(cipher);
+    return cipherText;
+}
+function CVentry(entry) {
+    let len =1;
+    let title="<p>";
+    title+=entry.from;
+    if (entry.to) {
+        title+='-'
+        title+=entry.to;
+        len=entry.to-entry.from;
+    }
+    title+="</p>"
+    return `<div style="height:${len*3}em"><div>${title}<a href="${entry.link}">${entry.name}</a></div></div>`
+}
+async function buildCV(key) {
+    const blob=await decrypt(key);
+    const data=await JSON.parse(blob);
+    let html='';
+    for (const key in data) {
+        const entry=data[key];
+        html+=CVentry(entry);
+    }
+    return html;
+}
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-
+const elem = document.getElementById("main");
 if(urlParams.has('key')){
     const key = urlParams.get('key');
-    console.log(key);
-    main(key);
+    buildCV(key).then((data)=>{
+        elem.innerHTML=data;
+    });
+    
 } else {
+    elem.innerHTML="access denied";
     console.log("no key");
 }
+// buildCV();   
